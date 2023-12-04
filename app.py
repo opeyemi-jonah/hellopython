e70f28db77e62b011fa048dc6590fe2540595e9e
opeyemi-jonah <gavrieljonah@gmail.com>
2023-12-04 01:41:06 -0700

    added new function.

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, Dockerized Flask App!'
 
+@app.route('/{name}')
+def hello_name(name):
+    return 'Hello, {}!'.format(name)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
